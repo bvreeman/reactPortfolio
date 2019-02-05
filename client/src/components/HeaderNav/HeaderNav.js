78ec65554f0368c9b7d7c45fbc20047eb42cc1af
bvreeman <bvreeman@gmail.com>
2019-02-05 17:03:33 -0600

    working on the header

--- client/src/components/HeaderNav/HeaderNav.js
+++ client/src/components/HeaderNav/HeaderNav.js
@@ -7,15 +7,13 @@ const HeaderNav = () => {
     return (
         <div className="sticky headerContainer">
             <nav className="navbar">
-                <div className="row navbar-header">
-                    <div className='col-md-3 col-xs-12 navbarLeft'>
-                        <NavLink to="/" className='navbarTitle'>Brandon Vreeman</NavLink>
-                    </div>
-                    <NavLink to="/" className="navbar-brand">About Me</NavLink>
-                    <NavLink to="/MyProjects" className="navbar-brand">My Projects</NavLink>
-                    <NavLink to='/ContactUsPage' className="navbar-brand">Contact Me</NavLink>
-                    <a className="navbarRight socialItems fb-ic ml-0" rel="noopener noreferrer" href="https://www.facebook.com/bvreeman/" target="_blank" style={{color: '#ffffff'}}><i className="fa fa-facebook white-text mr-lg-4"></i></a>
+                <div className='col-md-3 col-xs-12 navbarLeft'>
+                    <NavLink to="/" className='navbarTitle'>Brandon Vreeman</NavLink>
                 </div>
+                <NavLink to="/" className="navbar-brand">About Me</NavLink>
+                <NavLink to="/MyProjects" className="navbar-brand">My Projects</NavLink>
+                <NavLink to='/ContactUsPage' className="navbar-brand">Contact Me</NavLink>
+                <i className="navbarRight socialItems fab fa-facebook-f fa-3x" rel="noopener noreferrer" href="https://www.facebook.com/bvreeman/" target="_blank" />
             </nav>
         </div>
     )
