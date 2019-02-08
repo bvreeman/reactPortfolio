dc6997076de876da1eb5025b6b1e881417f5ae18
bvreeman <bvreeman@gmail.com>
2019-02-07 20:57:12 -0600

    trying to fix the spacing issue on the Project List

--- client/src/components/ContactFormSubmit/ContactFormSubmit.js
+++ client/src/components/ContactFormSubmit/ContactFormSubmit.js
@@ -7,12 +7,12 @@ class ContactFormSubmit extends React.PureComponent {
         return (
             <div className='ContactFormSubmit'>
                 <div className='row'>
-                    <div className='col-md-6 col-xs-12 submittedFormDiv'>
+                    <div className='col-md-12 col-xs-12 submittedFormDiv'>
                         <div className="contactSubmitHeader">
                             <h1>Thank you for submitting your message</h1>
                         </div>
                         <div className='contactSubmitParagraph'>
-                            <p>We will get back to you as soon as possible.</p>
+                            <p>I will get back to you as soon as possible.</p>
                         </div>
                         {/* <img className='contactSubmitLogo' src={logo} alt="Minnesota Valley Transport, New Ulm, MN"/> */}
                     </div>
