203cd97c6e95c294afa80f54b4dde6582454bf16
bvreeman <bvreeman@gmail.com>
2019-05-14 13:13:00 -0500

    added Link to the buttons to get them to work on heroku

--- client/src/components/ProjectsContactButtons/ProjectsContactButtons.js
+++ client/src/components/ProjectsContactButtons/ProjectsContactButtons.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import './ProjectsContactButtons.css'
+import {Link} from 'react-router-dom';
+
 
 class ProjectsContactButtons extends React.PureComponent {
     render () {
         return (
             <div>
                 <div className="row center-block">
-                    <a href="/MyProjects" className="linkButton">Click to view My Projects</a>
+                    <Link to="/MyProjects" className="linkButton">Click to view My Projects</Link>
                 </div>
                 <div className="row center-block">
-                    <a href="/ContactUsPage" className="linkButton">Click to Contact Me</a>
+                    <Link to="/ContactUsPage" className="linkButton">Click to Contact Me</Link>
                 </div>
             </div>
         )
