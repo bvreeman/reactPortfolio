4d989137152f5f3a16e00655aa10fe06eceb302e
bvreeman <bvreeman@gmail.com>
2019-05-14 13:25:36 -0500

    fixed social link

--- client/src/components/HeaderNav/HeaderNav.js
+++ client/src/components/HeaderNav/HeaderNav.js
@@ -1,6 +1,6 @@
 import React from "react";
 import './HeaderNav.css';
-import {NavLink} from 'react-router-dom';
+import {NavLink, Link} from 'react-router-dom';
 
 
 const HeaderNav = () => {
@@ -13,7 +13,7 @@ const HeaderNav = () => {
                 <NavLink to="/" className="navbar-brand">About Me</NavLink>
                 <NavLink to="/MyProjects" className="navbar-brand">My Projects</NavLink>
                 <NavLink to='/ContactUsPage' className="navbar-brand">Contact Me</NavLink>
-                <i className="navbarRight socialItems fab fa-facebook-f fa-3x" rel="noopener noreferrer" href="https://www.facebook.com/bvreeman/" target="_blank" />
+                <a className="navbarRight socialItems fab fa-facebook-f fa-3x" rel="noopener noreferrer" href="https://www.facebook.com/bvreeman/" target="_blank" style={{color: '#ffffff'}}><i className="fa fa-facebook white-text mr-lg-4"></i></a>
             </nav>
         </div>
     )
